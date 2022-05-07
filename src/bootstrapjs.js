65d42ee4f3c3d2037f02bb3feb8493309790afcd
unknown <mroldsma@gmail.com>
2022-05-07 03:29:07 -0300

    sustituyendo handlebars por ejs

--- src/bootstrapjs.js
+++
@@ -1,3 +0,0 @@
-const bs = require("../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js");
-
-module.exports = bs
--- src/index.js
+++ src/index.js
@@ -7,22 +7,16 @@ const path = require('path')
 // SETTINGS
 const app = express()
 const PORT = 5000
+
+// MIDDLEWARES
 app.use(express.static(path.join(__dirname, 'public')))
 
-
 app.set('views', path.join(__dirname, 'views'))
-app.engine('.hbs', handlebars.engine({
-  defaultLayout: 'main',
-  layoutsDir: path.join(app.get('views'), 'layouts'),
-  partialsDir: path.join(app.get('views'), 'partials'),
-  extname: '.hbs'
-}))
-app.set('view engine', '.hbs')
-
+app.set('view engine', 'ejs')
 
 // ROUTES
 app.get('/', (req, res)=>{
-  res.render('index', {rutadebootstrap: `${path.join(__dirname, '../node_modules/bootstrap/dist/js/bootstrap.min.js')}`})
+  res.render('index')
 })
 
 
