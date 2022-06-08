363f7be2e26e4db8946bd2bd53a9c9cc7c55116e
unknown <mroldsma@gmail.com>
2022-06-08 01:12:56 -0300

    agregado vercel.json2

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // MIDDLEWARES
-app.use(express.static(path.join(__dirname, "public")));
-app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "src", "public")));
+app.set("views", path.join(__dirname, "src", "views"));
 app.set("view engine", "ejs");
 
 // ROUTES
