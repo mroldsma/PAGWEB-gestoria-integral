5bbde15c1f83a934be89fb552bce6e0c292ffaf2
unknown <mroldsma@gmail.com>
2022-06-08 01:00:59 -0300

    agregado vercel.json

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
-// REQUIRING MODULES 
-const express = require('express')
-const path = require('path')
-
+// REQUIRING MODULES
+const express = require("express");
+const path = require("path");
 
 // SETTINGS
-const app = express()
-const PORT = 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 // MIDDLEWARES
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // ROUTES
-app.get('/', (req, res)=>{
-  res.render('index')
-})
-
-
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 // SERVER LISTENING
-app.listen(PORT, ()=>{
-  console.log('server listening on port: '+PORT)
-})
+app.listen(PORT, () => {
+  console.log("server listening on port: " + PORT);
+});
